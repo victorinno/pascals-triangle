c0505ab09ee7c0659ba576a54788c2e835508ae9
victorinno <rotc21@gmail.com>
2021-01-20 12:02:46 +0000

    improved

--- src/lib.rs
+++ src/lib.rs
@@ -4,34 +4,22 @@ pub struct PascalsTriangle {
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        if row_count == 0 {
-            return PascalsTriangle { rows: Vec::new() };
-        }
-
         let mut pascals_triangle = PascalsTriangle { rows: vec![] };
-
-        for i in 0..row_count {
-            if i == 0 {
-                pascals_triangle.rows.push(vec![1]);
-            } else {
-                let mut row = Vec::new();
-                let index: usize = (i - 1) as usize;
-                let before: &Vec<u32> = pascals_triangle.rows.get(index).expect("expected to have at least frist row");
-                for sum in 0..(before.len() + 1) {
-                    if sum == 0 || sum == before.len() {
-                        row.push(1);
-                    } else {
-                        row.push(before.get(sum).expect("expects A") + before.get(sum - 1).expect("expects B"));
-                    }
-                }
-                pascals_triangle.rows.push(row);
-            }
-        }
-
+        (0..row_count).for_each(|r| pascals_triangle.rows.push(PascalsTriangle::calc(r)));
         pascals_triangle
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
         Vec::from(self.rows.as_slice())
+    }
+
+    fn calc(row_count: u32) -> Vec<u32> {
+        let mut row: Vec<u32> = vec![1];
+        for p in 1..(row_count + 1) {
+            if let Some(&last) = row.last() {
+                row.push((last * (row_count + 1 - p)) / p)
+            }
+        }
+        row
     }
 }
